3585e76c14c3ab1d3b8ed54d0b5814f0d2004a45
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-05-03 14:31:09 +0300

    case  "contactId to String"

--- contacts.js
+++ contacts.js
@@ -10,20 +10,22 @@ async function listContacts() {
 }
 
 async function getContactById(contactId) {
+  const contactIdString = String(contactId);
   const contacts = await listContacts();
-  const result = contacts.find((item) => item.id === contactId);
+  const result = contacts.find((item) => item.id === contactIdString);
   return result || null;
 }
 
 async function removeContact(contactId) {
-    contacts = await listContacts();
-    const index = contacts.findIndex((item) => item.id === contactId)
-    if (index === -1) {
-        return null;
-    }
-    const [result] = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return result;
+  const contactIdString = String(contactId);
+  contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactIdString);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 }
 
 async function addContact({ name, email, phone }) {
