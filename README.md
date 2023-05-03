21f400ec603f14987f4bfb41843d6dd7aa7c1c11
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-05-03 14:04:32 +0300

    all works good

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CLI-application
+
+
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+node index.js --action="list"
+https://ibb.co/F0FMQPV
+
+# Отримуємо контакт по id
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+https://ibb.co/HYn84kk
+
+# Додаємо контакт
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+https://ibb.co/FqkQCvd
+
+# Видаляємо контакт
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+https://ibb.co/QPthgHm
---
+++ index.js
@@ -0,0 +1,31 @@
+const argv = require("yargs").argv;
+const contacts = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log("newContact: ", newContact);
+      break;
+
+    case "remove":
+      const removeBook = await contacts.removeContact(id);
+      console.log("removeBook: ", removeBook);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
